aa5eadb1e1a1276b590879b6d75e3d32df281d5f
Y-ostin <yostin.arequipa@tecsup.edu.pe>
2024-10-07 15:48:31 -0500

    junit

--- src/main/java/com/mycompany/proyectoprueba/ArrayTest.java
+++ src/main/java/com/mycompany/proyectoprueba/ArrayTest.java
@@ -3,11 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.proyectoprueba;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 /**
  *
  * @author User
  */
 public class ArrayTest {
+    @Test
+    public void testSize(){
+    }
     
+    @Test
+    public void testoString(){
+    }
+    
+    @Test
+    public void testSetElemental(){
+    }
 }
