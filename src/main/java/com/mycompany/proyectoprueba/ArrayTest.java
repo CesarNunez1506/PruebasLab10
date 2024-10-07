0f49eecae3b6e5653865559a4816515c9fd3db5a
CesarNunez1506 <cesar.nunez@tecsup.edu.pe>
2024-10-07 15:58:16 -0500

    Feat: ArrayTest

--- src/main/java/com/mycompany/proyectoprueba/ArrayTest.java
+++ src/main/java/com/mycompany/proyectoprueba/ArrayTest.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.proyectoprueba;
+
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
-/**
- *
- * @author User
- */
 public class ArrayTest {
     @Test
     public void testSize(){
