6097768650ca9d440dcd24f6ee42989bb3fe20f0
Y-ostin <yostin.arequipa@tecsup.edu.pe>
2024-10-07 16:01:13 -0500

    aa

--- src/main/java/com/mycompany/proyectoprueba/ArrayTest.java
+++ src/main/java/com/mycompany/proyectoprueba/ArrayTest.java
@@ -14,10 +14,17 @@ import static org.junit.Assert.*;
 public class ArrayTest {
     @Test
     public void testSize(){
+        int arr[]={1,2,3};
+        Array a= new Array(arr);
+        assertEquals(3,a.size());
     }
     
     @Test
     public void testoString(){
+        int arr[]={1,2,3};
+        Array a= new Array(arr);
+        assertEquals("1,2,3",a.toString());
+    
     }
     
     @Test
