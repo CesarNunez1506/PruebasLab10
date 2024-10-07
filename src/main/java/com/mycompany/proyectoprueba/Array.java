0d40841d3818b5e04ebe1184759ccdcdb503f37f
CesarNunez1506 <cesar.nunez@tecsup.edu.pe>
2024-10-07 15:41:50 -0500

    Feat: Array

--- src/main/java/com/mycompany/proyectoprueba/Array.java
+++ src/main/java/com/mycompany/proyectoprueba/Array.java
@@ -4,10 +4,27 @@
  */
 package com.mycompany.proyectoprueba;
 
-/**
- *
- * @author User
- */
+
 public class Array {
-    
+    int vec[];
+    public Array (int arr[]) {
+        vec = arr; 
+    }
+    public int size() {
+        return vec.length;
+    }
+    public String toString() {
+        int i;
+        String str="";
+        for(i=0;i<vec.length-1;i++)
+            str += vec[i] + ";";
+        str += vec[i];
+        return str;
+    }
+    public void setElementAt(int elem, int idx) {
+    vec[idx]=elem;
+    }
+    public int[] getArray(){
+        return vec;
+    }
 }
